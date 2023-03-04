d26405fa0d8b41c9a3f70f742ffbd1a72291727f
Jennifer <jennifer@Zipcoders-MacBook-Pro-9.local>
2023-03-03 21:35:09 -0500

    I have added changes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,31 @@
+import java.util.Scanner;
+
 /**
  * Created by iyasuwatts on 10/17/17.
  */
 public class Main {
 
-    public static void main(String[] args){
-        
-    }
-}
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int secretNumber = (int) (Math.random() * 100) + 1; //generate a # between 1 to 100
+        int guess;
+        int numGuesses = 0;
+
+        do {
+            System.out.println("Guess the number I'm thinking of. "
+                            + "It's between 1 and 100. ");
+            guess = input.nextInt();
+            numGuesses++;
+
+            if (guess < secretNumber) {
+                System.out.println("Too small!");
+            } else if (guess > secretNumber) {
+                System.out.println("Too large!");
+            } else {
+                System.out.println("Correct guess!");
+            }
+        } while (guess != secretNumber);
+        System.out.println("Number of guesses: " + numGuesses);
+    }}
+
